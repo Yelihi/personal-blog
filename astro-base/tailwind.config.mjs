4ceabcdf79b8459c6b3484600812246d06c7e482
Yelihi <Yelihi19@gmail.com>
2025-11-04 03:11:10 +0900

    feat: dark mode > tailwind selector & layout script

--- astro-base/tailwind.config.mjs
+++ astro-base/tailwind.config.mjs
@@ -3,5 +3,6 @@ import typography from '@tailwindcss/typography';
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue}'],
   theme: { extend: {} },
+  darkMode: 'selector',
   plugins: [typography],
 };
