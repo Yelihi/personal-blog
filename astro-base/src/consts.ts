2a9f326c0ef4f071159265a0869b430d8eb3b6f6
Yelihi <Yelihi19@gmail.com>
2025-11-18 10:07:32 +0900

    feat: head metadata 설정

--- astro-base/src/consts.ts
+++ astro-base/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = `Yelihi`;
+export const SITE_DESCRIPTION = `Yelihi의 블로그입니다. 개발 관련 글을 작성합니다.`;
